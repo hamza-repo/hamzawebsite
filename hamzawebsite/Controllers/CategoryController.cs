7c14a177633ad9281f9806054899da0fcfa3e039
hamza <110597391+Hamza-intern@users.noreply.github.com>
2023-12-20 15:08:28 +0300

    Add project files.

---
+++ hamzawebsite/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using hamzawebsite.Data;
+using hamzawebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hamzawebsite.Controllers
+{
+    public class CategoryController : Controller
+
+
+
+    {
+
+
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+        }
+
+
+    }
+}
